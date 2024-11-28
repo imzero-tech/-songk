91bacb63d07aa5ab32f741cfa991c8fa699e07a7
zero <zero@neowiz.com>
2024-11-28 18:03:16 +0900

    first create

---
+++ hunting-job/hunting-job/src/main/java/com/sonk/huntingjob/HuntingJobApplication.java
@@ -0,0 +1,13 @@
+package com.sonk.huntingjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuntingJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuntingJobApplication.class, args);
+	}
+
+}
---
+++ hunting-job/hunting-job/src/test/java/com/sonk/huntingjob/HuntingJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonk.huntingjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuntingJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
